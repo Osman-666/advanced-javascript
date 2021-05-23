faae78743eb33da8291dead7b4032403f1b55020
Osman Ali <yasinosman666@gmail.com>
2021-05-23 08:13:34 +0600

    i learned advanced javascript

---
+++ condition.js
@@ -0,0 +1,15 @@
+// const num=1;
+// if(num){
+//     console.log("it is true");
+// }
+// else{
+//     console.log("it is false");
+// }
+
+const name=" ";
+if(name){
+    console.log("it is true");
+}
+else{
+    console.log("it is false");
+}
