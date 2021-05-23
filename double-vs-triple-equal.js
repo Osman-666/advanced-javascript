f03b9eab8fb1976a19545f00c05b2569194c7819
Osman Ali <yasinosman666@gmail.com>
2021-05-23 15:07:53 +0600

    i learned the use of double equal and triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,10 @@
+//in this code i understood the deference between double equal(==) and triple equal(===).
+
+const first = 0;
+const second = false;
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
