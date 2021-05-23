ade6d3b9170f0da98098aacaa704c7e2e85aee48
Osman Ali <yasinosman666@gmail.com>
2021-05-23 08:36:28 +0600

    discussion between null Vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+let pakhi;
+
+function add(num1,num2){
+    console.log(num1+num2);
+    return num1;
+}
+
+function add(num1,num2) {
+    console.log(num1,num2);
+}
+
+const cow ={dakNam: "goru",leg: 4,head: 1};
+console.log(cow.haveChild);
+
+const fun = undefined;
+console.log(fun);
+
+const result = add(11);
+console.log(result);
+
